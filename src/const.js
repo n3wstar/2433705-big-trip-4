dd17e355a53e0a8cf98666913e865736b31ce1b6
andrey <yagunov02@bk.ru>
2024-05-20 23:04:42 +0500

    module5-task2

--- src/const.js
+++ src/const.js
@@ -16,6 +16,22 @@ const Mode = {
   EDITING: 'EDITING',
 };
 
+const SortType = {
+  DAY: 'day',
+  EVENT: 'event',
+  TIME: 'time',
+  PRICE: 'price',
+  OFFER: 'offer'
+};
+
+const EnabledSortType = {
+  [SortType.DAY]: true,
+  [SortType.EVENT]: false,
+  [SortType.TIME]: true,
+  [SortType.PRICE]: true,
+  [SortType.OFFER]: false
+};
+
 const MSEC_IN_SEC = 1000;
 const SEC_IN_MIN = 60;
 const MIN_IN_HOUR = 60;
@@ -24,4 +40,4 @@ const HOUR_IN_DAY = 24;
 const MSEC_IN_HOUR = MIN_IN_HOUR * SEC_IN_MIN * MSEC_IN_SEC;
 const MSEC_IN_DAY = HOUR_IN_DAY * MSEC_IN_HOUR;
 
-export {EMPTY_POINT, MSEC_IN_DAY, MSEC_IN_HOUR, Mode};
+export {EMPTY_POINT, MSEC_IN_DAY, MSEC_IN_HOUR, Mode, SortType, EnabledSortType};
--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -3,7 +3,27 @@ import AbstractView from '../framework/view/abstract-view.js';
 
 
 export default class SortView extends AbstractView {
-  get template() {
-    return CreateSortMarkup();
+  #currentSortType = null;
+  #handleSortTypeChange = null;
+
+  constructor(currentSortType, handleSortTypeChange) {
+    super();
+    this.#currentSortType = currentSortType;
+    this.#handleSortTypeChange = handleSortTypeChange;
+
+    this.element.addEventListener('click', this.#sortTypeChangeHandler);
   }
+
+  get template() {
+    return CreateSortMarkup(this.#currentSortType);
+  }
+
+  #sortTypeChangeHandler = (evt) => {
+    if (evt.target.tagName !== 'INPUT') {
+      return;
+    }
+
+    evt.preventDefault();
+    this.#handleSortTypeChange(evt.target.dataset.sortType);
+  };
 }
